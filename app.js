64f7a6633095bb423da5e4380345825c7f5f83c4
unknown <tongmu@tiaozhan.com>
2016-05-30 16:25:00 +0800

    Bug: fix package.json, add a demo in app.js

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ var app = require('xtpl/lib/koa')(require('koa')(),{
 });
 
 app.use(route.get('/:name', function*(name) {
-    yield this.render('index',{title: 1});
+    yield this.render('index', {title: name});
+}));
+
+app.use(route.get('/', function*(name) {
+    yield this.render('index', {title: 'index'});
 }));
 
 // 所有没有命中的路由
 app.use(function *() {
-    this.throw("404 not founr", 404);
+    this.throw("404 not found, root path only now.", 404);
 });
 
 app.listen(3000);
